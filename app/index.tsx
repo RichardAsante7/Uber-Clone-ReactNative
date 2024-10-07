0b16994a7ea5dcb8e6c6d1eb62c30af70e460b35
Richard Mylo Asante <richardmyloasante@gmail.com>
2024-10-07 21:35:45 +0000

    project setup and cleanup

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView 
+        style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+        }}
+    >
+        <Text>Application</Text>
+    </SafeAreaView>
+    
+   
+  );
+}
